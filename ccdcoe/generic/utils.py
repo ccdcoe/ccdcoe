4821838d3b30ccfd25ba82ca104ec625851926f5
lisplesssnake <17860089+lisplesssnake@users.noreply.github.com>
2026-06-26 14:50:49 +0300

    fix

--- ccdcoe/generic/utils.py
+++ ccdcoe/generic/utils.py
@@ -60,16 +60,18 @@ def getenv_str(
     mandatory: bool = False,
     config_file_location: str = None,
 ):
-    raw = os.getenv(name) or default # treat empty str as unset
+    raw = os.getenv(name)
 
-    if mandatory:
-        if raw == __MANDATORY_VALUE__:
-            raise ValueError(
-                f"Mandatory variable: {name} is not set!"
-                if config_file_location is None
-                else f"Mandatory variable: {name} is not set; set this variable in the config file "
-                f"located at: {config_file_location}!"
-            )
+    if raw is None or raw.strip() == "":
+        raw = default
+
+    if mandatory and raw == __MANDATORY_VALUE__:
+        raise ValueError(
+            f"Mandatory variable: {name} is not set!"
+            if config_file_location is None
+            else f"Mandatory variable: {name} is not set; set this variable in the config file "
+                 f"located at: {config_file_location}!"
+        )
 
     return raw
 
